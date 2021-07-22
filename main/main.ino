0e705c5da009a64338335311e6b9ebdad237cf70
Sunirban Ranjit <77260637+SunirbanRanjit@users.noreply.github.com>
2021-07-22 02:07:58 -0700

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -5,13 +5,7 @@
 
 #include <map>
 
-#define WIFI_SSID         "WIFI_SSID"    
-#define WIFI_PASS         "WIFI_PASSKEY"
-#define APP_KEY           "APP_KEY"      
-#define APP_SECRET        "APP_SECRET"   
-
-#define device_ID_1   "DEVICE_ID"
-#define device_ID_2   "DEVICE_ID"
+#include "config.h"
 
 #define RelayPin1 D2 
 #define RelayPin2 D5 
