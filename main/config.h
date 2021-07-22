c0763ee16e80f9830904f1dada337f2c9fdbd4bd
Sunirban Ranjit <77260637+SunirbanRanjit@users.noreply.github.com>
2021-07-22 02:15:48 -0700

    Create config.h

---
+++ main/config.h
@@ -0,0 +1,8 @@
+
+#define WIFI_SSID         "WIFI_SSID"    
+#define WIFI_PASS         "WIFI_PASSKEY"
+#define APP_KEY           "APP_KEY"      
+#define APP_SECRET        "APP_SECRET"   
+
+#define device_ID_1   "DEVICE_ID"
+#define device_ID_2   "DEVICE_ID"
